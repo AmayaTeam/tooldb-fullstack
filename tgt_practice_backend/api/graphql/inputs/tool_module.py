45acd81c89f57e294ccde07092c29d29f5566dd3
Stepan Tischenko <stepantishhen@mail.ru>
2024-07-29 11:23:21 +0300

    backend fix

--- tgt_practice_backend/api/graphql/inputs/tool_module.py
+++ tgt_practice_backend/api/graphql/inputs/tool_module.py
@@ -3,22 +3,12 @@ import graphene
 
 class CreateToolModuleInput(graphene.InputObjectType):
     r_module_type_id = graphene.UUID(required=True)
-    sn = graphene.String(rpassequired=True)
-    dbsn = graphene.String(required=True)
+    sn = graphene.String(required=True)
+    dbsn = graphene.String(required=False)
     dbtname = graphene.String(required=True)
     dbdate = graphene.Date()
     dbversion = graphene.String()
     dbcomment = graphene.String()
-    dbtlength = graphene.Float()
-    dbtweight = graphene.Float()
-    dbtmax_od = graphene.Float()
-    dbtmax_od_collapsed = graphene.Float()
-    dbtmax_od_opened = graphene.Float()
-    dbtimage_h_shift = graphene.Float()
-    dbtimage_h_scale = graphene.Float()
-    dbtimage_h_y1 = graphene.Float()
-    dbtimage_h_y2 = graphene.Float()
-    dbtcomp_str = graphene.Float()
     image = graphene.String()
 
 
@@ -32,16 +22,6 @@ class UpdateToolModuleInput(graphene.InputObjectType):
     dbdate = graphene.Date()
     dbversion = graphene.String()
     dbcomment = graphene.String()
-    dbtlength = graphene.Float()
-    dbtweight = graphene.Float()
-    dbtmax_od = graphene.Float()
-    dbtmax_od_collapsed = graphene.Float()
-    dbtmax_od_opened = graphene.Float()
-    dbtimage_h_shift = graphene.Float()
-    dbtimage_h_scale = graphene.Float()
-    dbtimage_h_y1 = graphene.Float()
-    dbtimage_h_y2 = graphene.Float()
-    dbtcomp_str = graphene.Float()
     image = graphene.String()
 
 
