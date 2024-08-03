9065eb446b3b1cdd64e4b953714bcd517f5d0e47
MishaBlin <mikhail_kalinin_a@mail.ru>
2024-08-04 00:14:55 +0500

    Import image feature

---
+++ tgt_practice_frontend/src/graphql/mutations/ToolModule/updateToolModule.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const UPDATE_TOOL_MODULE = gql`
+  mutation UpdateToolModule($input: UpdateToolModuleInput!) {
+    updateToolModule(input: $input) {
+      toolModule {
+        image
+        }
+    }
+}
+`;
